9feb196646c7f5081651ce5f2c23f3a3094a2428
anjin <anjin720jin@naver.com>
2022-11-07 15:11:13 +0900

    Initial Commit

---
+++ Bakering/BakeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeringApp.swift
+//  Bakering
+//
+//  Created by 이안진 on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct BakeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
